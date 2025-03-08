4327d9f92d0ea38806daa1cb94972dd0e1f15a21
devyagnesh <con.boringmacho@gmail.com>
2025-03-08 18:04:25 +0530

    kiba initial page design

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $("nav").addClass(
+        "shadow-md bg-white transition-all duration-300 fixed top-0 z-[1000]"
+      );
+    } else {
+      $("nav").removeClass("shadow-md fixed top-0 transparent");
+    }
+  });
+
+  $("#mobile-menu-btn").click(function () {
+    $("#mobile-menu").slideToggle();
+  });
+
+  $("#mobile-dropdown-btn").click(function () {
+    $("#mobile-dropdown").slideToggle();
+  });
+
+  showCollectionSlide();
+});
+
+function showCollectionSlide() {
+  let index = 0;
+  const slides = $("#collection-slider .slide");
+  const totalSlides = slides.length;
+  const slider = $("#collection-slider");
+
+  function updateSlider() {
+    const translateValue = -index * 100 + "%";
+    slider.css("transform", "translateX(" + translateValue + ")");
+  }
+
+  $("#next").click(function () {
+    index = (index + 1) % totalSlides;
+    updateSlider();
+  });
+
+  $("#prev").click(function () {
+    index = (index - 1 + totalSlides) % totalSlides;
+    updateSlider();
+  });
+}
